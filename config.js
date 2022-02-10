a112698041bbd893e46ad14c5ebb3247f1314bc6
AndryQnn <98902085+AndryQnn@users.noreply.github.com>
2022-02-10 15:15:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ global.APIKeys = {
 	'https://zeks.me': 'Alphabott',
 }
 
-global.owner = ['62887435047326','918156874290']
+global.owner = ['6285894148101','6281332657531']
 global.packname = setting.botname
 global.author = setting.ownername
 global.alphaVersion = [2, 2143, 3]
